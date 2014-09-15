489666a517df167dff2a6b50e80d91b9058ec38c
Grant Timmerman <granttimmerman@gmail.com>
2014-09-15 19:50:47 -0400

    Add tests for remove and isEmpty

--- test/test.js
+++ test/test.js
@@ -8,6 +8,7 @@ function objEqual (obj1, obj2) {
 }
 
 describe('Hexgrid', function () {
+  // Hexgrid()
   describe('constructor', function () {
     it('should be good', function () {
       var grid = new Hexgrid({
@@ -17,6 +18,7 @@ describe('Hexgrid', function () {
     });
   });
 
+  // Hexgrid.add
   describe('add', function () {
     it('should add hexcells to the grid', function () {
       var grid = new Hexgrid();
@@ -29,6 +31,35 @@ describe('Hexgrid', function () {
       grid.add(new Hexcell(new Point(), { data: 'cell1' }));
       grid.add(new Hexcell(new Point(), { data: 'cell2' }));
       assert(objEqual(grid.get(new Point(0, 0)).data, { data: 'cell2' }));
+    });
+  });
+
+  // Hexcell.remove
+  describe('remove', function () {
+    it('should remove properly', function () {
+      var grid = new Hexgrid();
+      grid.add(new Hexcell(new Point()));
+      grid.remove(new Hexcell(new Point()));
+      assert(grid.isEmpty(new Point()));
+    });
+
+    it('should not blow up if we try to remove a cell that is empty', function () {
+      var grid = new Hexgrid();
+      grid.remove(new Point());
+    });
+  });
+
+  // Hexcell.isEmpty
+  describe('isEmpty', function () {
+    it('should be true when there is no hexagon at a cell', function () {
+      var grid = new Hexgrid();
+      assert(grid.isEmpty(new Point()));
+    });
+
+    it('should be false when there is a hexagon at a cell', function () {
+      var grid = new Hexgrid();
+      grid.add(new Hexcell(new Point()));
+      assert(!grid.isEmpty(new Point()));
     });
   });
 });
