5444e7fc1673fd60d91b9df45111397e0b134999
Grant Timmerman <granttimmerman@gmail.com>
2014-09-15 19:44:13 -0400

    Add unit tests for add method

--- test/test.js
+++ test/test.js
@@ -3,6 +3,10 @@ var assert = require('assert');
 var Hexgrid = require('../');
 var Hexcell = require('hexcell');
 
+function objEqual (obj1, obj2) {
+  return JSON.stringify(obj1) === JSON.stringify(obj2);
+}
+
 describe('Hexgrid', function () {
   describe('constructor', function () {
     it('should be good', function () {
@@ -10,6 +14,21 @@ describe('Hexgrid', function () {
         size: 20
       });
       assert.equal(grid.size, 20);
+    });
+  });
+
+  describe('add', function () {
+    it('should add hexcells to the grid', function () {
+      var grid = new Hexgrid();
+      grid.add(new Hexcell(new Point(0, 0), { data: 'cell1' }));
+      assert(objEqual(grid.get(new Point(0, 0)).data, { data: 'cell1' }));
+    });
+
+    it('should replace existing hexcells', function () {
+      var grid = new Hexgrid();
+      grid.add(new Hexcell(new Point(), { data: 'cell1' }));
+      grid.add(new Hexcell(new Point(), { data: 'cell2' }));
+      assert(objEqual(grid.get(new Point(0, 0)).data, { data: 'cell2' }));
     });
   });
 });
